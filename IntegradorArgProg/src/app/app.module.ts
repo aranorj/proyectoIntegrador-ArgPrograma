fbde656ca92bd46c38045c6478716dbd20a22e8b
Your Name <you@example.com>
2022-06-19 11:12:33 -0300

    styles: add 'trayectoria' section

--- IntegradorArgProg/src/app/app.module.ts
+++ IntegradorArgProg/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoArgPComponent } from './components/logo-arg-p/logo-arg-p.component'
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { TrayectoriaComponent } from './components/trayectoria/trayectoria.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoArgPComponent,
     RedesComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    TrayectoriaComponent
   ],
   imports: [
     BrowserModule,
